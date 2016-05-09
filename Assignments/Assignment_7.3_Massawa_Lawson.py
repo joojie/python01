2c453ba3f81b19ada0f586fb2e0aa8694afc4bfc
Massawa Lawson <sobrosowa@gmail.com>
2016-05-09 18:13:35 -0400

    Assignment 7.3

---
+++ Assignments/Assignment_7.3_Massawa_Lawson.py
@@ -0,0 +1,16 @@
+init = raw_input('Enter Filename: ')
+if init is not None:
+	fhand=open('mbox-short.txt.txt')
+count=0
+spamAVG=0
+for line in fhand:
+    words=line.split()
+    if line.startswith('X-DSPAM-Confidence:'):
+		count +=1
+		spamAVG=spamAVG+float(words[1])
+    else:
+        continue
+if count is 0: print "'X-DSPAM-Confidence:    0.8475 occurence':" ,count
+else:
+	spamAVG=spamAVG/count
+	print "Count=",count,"Avg X-DSPAM-Confidence:" ,spamAVG
